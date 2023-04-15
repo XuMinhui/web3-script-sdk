5bf1a00de64c54c19836eed30c05e528de0949a4
Homie_Xu <homie_xu@163.com>
2023-04-16 03:34:26 +0800

    ✨feat: optimized

--- examples/multicall.ts
+++ examples/multicall.ts
@@ -1,14 +1,17 @@
+import invariant from "tiny-invariant"
 import { ChainState } from "../src/class/ChainState"
 import { CurrencyErc20 } from "../src/class/Currency"
 import { Multicall } from "../src/class/Multicall"
 import { bn_fromWei } from "../src/utils"
 import { SupportChain, ZERO_ADDRESS, multicallAddress } from "../src/web3"
+import { Web3Checker } from "../src/class/Checker"
 
 const runMulticall = async () => {
     const ethereum = new ChainState(SupportChain.Ethereum)
     const multicall = new Multicall(ethereum, multicallAddress[SupportChain.Ethereum])
 
     const ethereumUsdtAddress = '0xdAC17F958D2ee523a2206206994597C13D831ec7'
+    invariant(Web3Checker.isAddress(ethereumUsdtAddress), `should received a address`)
     const usdt = new CurrencyErc20(SupportChain.Ethereum, ethereumUsdtAddress)
     await usdt.initialize()
 
@@ -21,11 +24,16 @@ const runMulticall = async () => {
         [ethereumUsdtAddress, usdt.contract.interface.encodeFunctionData('balanceOf', [ZERO_ADDRESS])],
     ])
 
-    const name = usdt.contract.interface.decodeFunctionResult('name', decodedResults[0]).toString()
-    const totalSupply = usdt.contract.interface.decodeFunctionResult('totalSupply', decodedResults[1]).toString()
-    const balance = usdt.contract.interface.decodeFunctionResult('balanceOf', decodedResults[2]).toString()
+    // const name = usdt.contract.interface.decodeFunctionResult('name', decodedResults[0]).toString()
+    // const totalSupply = usdt.contract.interface.decodeFunctionResult('totalSupply', decodedResults[1]).toString()
+    // const balance = usdt.contract.interface.decodeFunctionResult('balanceOf', decodedResults[2]).toString()
+    const [name, totalSupply, balance] = Multicall.decode(decodedResults, [
+        [usdt.contract, 'name'],
+        [usdt.contract, 'totalSupply', 'number', usdt.decimals],
+        [usdt.contract, 'balanceOf', 'number', usdt.decimals],
+    ])
 
-    console.log(bn_fromWei(totalSupply, usdt.decimals), name, balance)
+    console.log(totalSupply, name, balance)
 }
 
 runMulticall()
--- src/class/Multicall.ts
+++ src/class/Multicall.ts
@@ -2,6 +2,9 @@ import { BigNumber, ethers } from "ethers";
 import { SupportChain } from "../web3";
 import { Multicall__factory } from "../web3/types";
 import { ChainState } from "./ChainState";
+import { number } from "joi";
+import { bn_fromWei, bn_parseWei } from "../utils";
+import invariant from "tiny-invariant";
 
 type MulticallCallsFragmentInterface = ([string, string] | { target: string, callData: string })[]
 
@@ -17,9 +20,6 @@ export class Multicall {
         this.multicall = Multicall__factory.connect(multicallAddr, this.provider)
     }
 
-    decode(decodedResults: string[], serializtion: [ethers.Contract, string, ('string' | 'number' | 'boolean')?, number?][]) {
-        
-    }
 
     async call(calls: MulticallCallsFragmentInterface): Promise<[BigNumber, string[]]> {
         const multicallData = Multicall.encodeFunctionData(calls)
@@ -30,6 +30,24 @@ export class Multicall {
 
         const [_blockNumber, decodedResults] = Multicall.multicallInterface.decodeFunctionResult('aggregate', results)
         return [_blockNumber, decodedResults]
+    }
+
+    static decode(decodedResults: string[], serializtions: [ethers.Contract, string, ('string' | 'number' | 'boolean')?, number?][]) {
+        invariant(decodedResults.length >= serializtions.length, `decodedResults length must then serializtions length`)
+        const results = serializtions.map((serializtion, index) => {
+            const [contract, method, format, decimals] = serializtion
+            const data = contract.interface.decodeFunctionResult(method, decodedResults[index]).toString()
+            switch (format) {
+                case 'number':
+                    return decimals ? bn_fromWei(data, decimals) : Number(data)
+                case 'boolean':
+                    return Boolean(data)
+                default:
+                    return decimals ? bn_parseWei(data, decimals) : data
+            }
+        })
+
+        return results
     }
 
     static encodeFunctionData(calls: MulticallCallsFragmentInterface) {
